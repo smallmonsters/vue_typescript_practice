c0f49d8e33fd2be34f3e4d0f0841df9d0655aa0c
13657618017@163.com <13657618017@163.com>
2019-10-18 22:18:09 +0800

    添加url-loader、file-loader

--- src/asset/js/about.js
+++ src/asset/js/about.js
@@ -1,7 +1,13 @@
 import "../css/base.css"
 import "../css/about.css"
+import img from "../image/abc.jpeg"
 import LimitFn from "@/util/LimitFn"
 import _ from "lodash"
+console.log(img, document.getElementById("img"))
+document.onload = function () {
+  document.getElementById("img").attr("src", img)
+}
+
 /**
  * 异步引入
  import("lodash").then((_) => {
